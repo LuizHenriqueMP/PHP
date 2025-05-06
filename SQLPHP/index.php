b0fc8fd34c807abbadf34eaacc0e2ff6258af22b
LuizHenriqueMP <luizhenriquempmngk@gmail.com>
2025-05-06 20:56:42 -0300

    Parte primária do projeto concluída
    Terminada a parte de conversão dos dados do arquivo JSON para o banco de dados SQL

--- SQLPHP/index.php
+++ SQLPHP/index.php
@@ -3,9 +3,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "pokedex";
 
 # Connects on the SQL server
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 # Check the connection
 if(!$conn){
@@ -22,9 +23,48 @@ if($conn->query($sql) === TRUE){
 }else{
     echo "Error creating database: ".$conn->error;
 }
+
+$sql = "
+CREATE TABLE pokemon(
+id INT AUTO_INCREMENT PRIMARY KEY,
+num INT(11),
+name VARCHAR(30),
+imgurl VARCHAR(100),
+type1 VARCHAR(30),
+type2 VARCHAR(30)
+)";
 */
 
+$id = 0;
+$num = 0;
+$name = "";
+$imgurl = "";
+$type1 = "";
+$type2 = "";
 
+$pokedexkanto = json_decode(file_get_contents("json/pokedex.json"), true);
+
+foreach($pokedexkanto["pokemons"] as $x){
+    $id = (int) $x["id"];
+    $num = (int) $x["num"];
+    $name = $x["name"];
+    $imgurl = $x["img"];
+    $type1 = $x["type"][0];
+    if(isset($x["type"][1])){
+        $type2 = $x["type"][1];
+    }else{
+        $type2 = "Nenhum";
+    }
+    
+    $sql = "INSERT INTO pokemon(id, num, name, imgurl, type1, type2) VALUES ($id, $num, '$name', '$imgurl', '$type1', '$type2')";
+
+    if($conn->query($sql) === TRUE){
+        echo "<br>";
+        echo "$name added successfully!";
+    }else{
+        echo "Error receiving the command: ".$conn->error;
+    }
+}
 
 $conn->close();
 ?>
