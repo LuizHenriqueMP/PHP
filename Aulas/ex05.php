a5068e9c40c9854948d0165f042dbb43b912ac5d
LuizHenriqueMP <98365070+LuizHenriqueMP@users.noreply.github.com>
2025-06-25 15:33:29 -0300

    Update
    Começo da atividade para exibir o cantor que o usuário quer

--- Aulas/ex05.php
+++ Aulas/ex05.php
@@ -12,10 +12,16 @@ echo "<br>";
 
 // 6. Faça uma página PHP que exiba os números pares entre 349 e 401.
 
-for($i=0; 349 < $i < 401; $i++){
+for($i=349; $i<401 ;$i++){
     if(!is_float($i/2)){
         echo $i. "<br>";
     }
 }
+
+echo "<br>";
+
+$multiplo = array("vermelho", "verde", 42 => "azul", "amarelo" => array("maçã", 9 => "pera", "banana", "laranja" => array("cão", "gato", "iguana")));
+
+echo $multiplo["amarelo"]["laranja"][2];
 
 ?>
